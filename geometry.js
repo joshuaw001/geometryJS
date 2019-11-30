08c805cf12e84864eb90bd9d6c4afe5f6d6919da
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-11-30 16:24:03 -0600

    create Circle class

--- geometry.js
+++ geometry.js
@@ -1,4 +1,4 @@
-//name------->geometryJS:
+//name------->geometryJS
 //author----->joshuaw001
 //type------->API
 //designer--->joshuaw001
@@ -19,5 +19,29 @@ class Square {
     function findSideLength() {
         return math.sqrt(this.findArea())
     }
-    
+    function findPerimeter() {
+        return this.length * 2
+    }
+}
+
+class Circle {
+    constructor(r) {
+        this.radius   = r
+        this.diameter = this.radius * 2
+        this.pi       = 3.14159265358979
+        this.tau      = this.pi * 2
+        this.perfect  = true
+    }
+    function findRadius() {
+        return this.radius
+    }
+    function findArea() {
+        return (this.pi * (this.findRadius() * this.findRadius()))
+    }
+    function findDiameter() {
+        return this.diameter
+    }
+    function findCircumference() {
+        return this.tau * this.radius
+    }
 }
