38a144d5946230d9551774a45de939dd131004c6
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-08-06 14:05:28 -0500

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -1,68 +1,8 @@
-//name------->geometryJS
-//author----->joshuaw001
-//type------->API
-//designer--->joshuaw001
-//language--->javascript
-//use-------->HTML5 canvas
-//developer-->joshuaw001
-//storage---->github
-//file------->geometry.js
-//specialty-->geometry
+/*
+ * Project Name: geometryJS
+ * Author      : joshuaw001
+ * Discription : a geometry library for html5 canvas, made with javascript.
+ * Version     : v1.1 beta
+ * See Also    : algebraJS
+ */
 
-class Square {
-    constructor(side) {
-        this.length = side
-    }
-    function findArea() {
-        return this.length * this.length
-    }
-    function findSideLength() {
-        return math.sqrt(this.findArea())
-    }
-    function findPerimeter() {
-        return this.length * 2
-    }
-}
-
-class Circle {
-    constructor(r) {
-        this.radius   = r
-        this.diameter = this.radius * 2
-        this.pi       = 3.14159265358979
-        this.tau      = this.pi * 2
-        this.perfect  = true
-    }
-    function findRadius() {
-        return this.radius
-    }
-    function findArea() {
-        return (this.pi * (this.findRadius() * this.findRadius()))
-    }
-    function findDiameter() {
-        return this.diameter
-    }
-    function findCircumference() {
-        return this.pi * this.findDiameter()
-    }
-    function makeOval() {
-        this.perfect = false
-    }
-}
-
-class RightTriangle {
-    constructor(legs,dists) {
-        this.a           = legs[0]
-        this.b           = legs[1]
-        this.c2          = ((this.a * this.a) + (this.b * this.b))
-        this.c           = math.sqrt(this.c2)
-        this.distA       = dists[0]
-        this.distB       = dists[1]
-        this.distC       = math.sqrt(this.c2)
-        this.midpoints   = {"a":this.distA / 2,"b":this.distB / 2,"c":this.distC / 2}
-        this.midAC       = {"slope":0,"distance":(this.distB / 2)}
-    }
-    function findHypotenusePower2() {
-        return this.c2
-    }
-
-}
